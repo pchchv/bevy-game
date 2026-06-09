01aa9b11d0b3225c597ebe228cf08e6914158f6c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-09 13:18:01 +0000

    refactor: move cameta setup to a separate function

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,12 @@ mod player;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, move_player) 
+        .add_systems(Startup, setup_camera)
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
     commands.spawn(Camera2d);	
-
-    commands.spawn((
-        Text2d::new("@"),
-        TextFont {
-            font_size: 12.0,	
-            font: default(),
-            ..default()
-        },
-        TextColor(Color::WHITE),
-        Transform::from_translation(Vec3::ZERO),
-        Player,
-    ));
 }
 
 fn move_player(
