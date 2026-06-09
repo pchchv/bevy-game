3fbf5dfa676ca1bbfe3d4956ba1114787b105146
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-09 13:18:33 +0000

    feat: add assets plugin

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,17 @@ mod player;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::WHITE)) // We have updated the bg color to white
+        .add_plugins(
+            DefaultPlugins.set(AssetPlugin {
+                file_path: "src/assets".into(),
+                ..default()
+            }),
+        )
         .add_systems(Startup, setup_camera)
         .run();
 }
 
 fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2d);	
-}
-
-fn move_player(
-    input: Res<ButtonInput<KeyCode>>,
-    time: Res<Time>,
-    mut player_transform: Single<&mut Transform, With<Player>>, 
-) {
-    let mut direction = Vec2::ZERO;
-    if input.pressed(KeyCode::ArrowLeft) {
-        direction.x -= 1.0;
-    }
-    if input.pressed(KeyCode::ArrowRight) {
-        direction.x += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowUp) {
-        direction.y += 1.0;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        direction.y -= 1.0;
-    }
-
-    if direction != Vec2::ZERO {
-        let speed = 300.0; // pixels per second
-        let delta = direction.normalize() * speed * time.delta_secs();
-        player_transform.translation.x += delta.x;
-        player_transform.translation.y += delta.y;
-    }
+    commands.spawn(Camera2d);
 }
