9318d9163f31045b8ae65937a03ff4214d7c26b9
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-09 11:29:47 +0000

    feat: add a new Entity

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,17 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-	commands.spawn(Camera2d);
+    commands.spawn(Camera2d);	
+
+    commands.spawn((
+        Text2d::new("@"),
+        TextFont {
+            font_size: 12.0,	
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::from_translation(Vec3::ZERO),
+        Player,
+    ));
 }
