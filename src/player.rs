ce5fb455c3fcdb19aa4a1c3f828627277300527f
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-10 11:58:39 +0000

    feat(player): create the Player Plugin

--- src/player.rs
+++ src/player.rs
@@ -166,3 +166,12 @@ pub fn animate_player(
         timer.reset();
     }
 }
+
+pub struct PlayerPlugin;
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, spawn_player)
+            .add_systems(Update, (move_player, animate_player));
+    }
+}
