1316f17298d98983f21b06468fbf278a63d89689
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-15 14:01:35 +0000

    feat(map): implement spawnable asset

---
+++ src/map/assets.rs
@@ -0,0 +1,14 @@
+use bevy::{prelude::*, sprite::Anchor};
+use bevy_procedural_tilemaps::prelude::*;
+
+#[derive(Clone)]
+pub struct SpawnableAsset {
+    /// Name of the sprite inside our tilemap atlas
+    sprite_name: &'static str,
+    /// Offset in grid coordinates (for multi-tile objects)
+    grid_offset: GridDelta,
+    /// Offset in world coordinates (fine positioning)
+    offset: Vec3,
+    /// Function to add custom components (like collision, physics, etc.)
+    components_spawner: fn(&mut EntityCommands),
+}
