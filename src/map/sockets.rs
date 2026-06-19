d4cb46316e5d1433622a35d50f1dc56cb36af606
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-19 12:44:42 +0000

    feat(map): add grass sockets

--- src/map/sockets.rs
+++ src/map/sockets.rs
@@ -6,6 +6,15 @@ pub struct DirtLayerSockets {
     pub material: Socket,       // What dirt connects to horizontally
 }
 
+pub struct GrassLayerSockets {
+    pub layer_up: Socket,
+    pub layer_down: Socket,
+    pub material: Socket,
+    pub void_and_grass: Socket,
+    pub grass_and_void: Socket,
+    pub grass_fill_up: Socket,
+}
+
 pub struct TerrainSockets {
     pub dirt: DirtLayerSockets,
 }
