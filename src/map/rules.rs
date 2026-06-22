610819e763bc89175d8458361e8e7b2bdb05e03c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-22 14:19:08 +0000

    refactor(map): implement the props layer function call

--- src/map/rules.rs
+++ src/map/rules.rs
@@ -634,6 +634,9 @@ pub fn build_world() -> (Vec<Vec<SpawnableAsset>>, ModelCollection<Cartesian3D>,
     // Build water layer
     build_water_layer(&mut terrain_model_builder, &terrain_sockets, &mut socket_collection);
 
+    // Build props layer
+    build_props_layer(&mut terrain_model_builder, &terrain_sockets, &mut socket_collection);
+
     let (assets, models) = terrain_model_builder.into_parts();
 
     (assets, models, socket_collection)
