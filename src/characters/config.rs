56e71e83ad97712ecb919ffc3fd598b80855d23f
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-23 16:15:22 +0000

    feat(characters): add AnimationDefinition

--- src/characters/config.rs
+++ src/characters/config.rs
@@ -8,3 +8,11 @@ pub enum AnimationType {
     Run,
     Jump
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AnimationDefinition {
+    pub start_row: usize,
+    pub frame_count: usize,
+    pub frame_time: f32,
+    pub directional: bool, // true = 4 rows (one per direction), false = 1 row
+}
