5c2b46c04e797e9c92bed2201e3a30d406c22f6c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-25 14:02:52 +0000

    feat(characters): add CharactersListResource that stores the handle to loaded characters

--- src/characters/spawn.rs
+++ src/characters/spawn.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use crate::characters::animation::*;
+use crate::characters::config::{CharacterEntry, CharactersList};
 use crate::characters::movement::Player;
 
 const PLAYER_SCALE: f32 = 0.8;
@@ -8,4 +9,9 @@ const PLAYER_Z_POSITION: f32 = 20.0;
 #[derive(Resource, Default)]
 pub struct CurrentCharacterIndex {
     pub index: usize,
+}
+
+#[derive(Resource)]
+pub struct CharactersListResource {
+    pub handle: Handle<CharactersList>,
 }
