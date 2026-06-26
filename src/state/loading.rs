7ef73f2ed188beb27629d811cb518cf151f08a25
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-26 14:01:55 +0000

    feat(state): implement spawn_loading_screen function

--- src/state/loading.rs
+++ src/state/loading.rs
@@ -5,3 +5,29 @@ pub struct LoadingScreen;
 
 #[derive(Component)]
 pub struct LoadingText;
+
+pub fn spawn_loading_screen(mut commands: Commands) {
+    commands.spawn((
+        LoadingScreen,
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        BackgroundColor(Color::srgb(0.1, 0.1, 0.15)),
+    )).with_children(|parent| {
+        parent.spawn((
+            LoadingText,
+            Text::new("Loading..."),
+            TextFont {
+                font_size: 48.0,
+                ..default()
+            },
+            TextColor(Color::WHITE),
+        ));
+    });
+    
+    info!("Loading screen spawned");
+}
