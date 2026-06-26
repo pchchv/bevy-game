0906196455ae3d7f4a56b1d42dbef42c4150e74c
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-26 14:07:04 +0000

    feat(state): implement check_assets_loaded function

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,3 +1,24 @@
 mod game_state;
 mod loading;
 mod pause;
+
+use bevy::prelude::*;
+use crate::characters::spawn::CharactersListResource;
+use crate::characters::config::CharactersList;
+
+pub use game_state::GameState;
+
+fn check_assets_loaded(
+    characters_list_res: Option<Res<CharactersListResource>>,
+    characters_lists: Res<Assets<CharactersList>>,
+    mut next_state: ResMut<NextState<GameState>>,
+) {
+    let Some(res) = characters_list_res else {
+        return;
+    };
+    
+    if characters_lists.get(&res.handle).is_some() {
+        info!("Assets loaded, transitioning to Playing!");
+        next_state.set(GameState::Playing);
+    }
+}
