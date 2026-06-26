9ea34c4a45d6dc86b94cf9646988517523463059
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-26 14:04:19 +0000

    feat(state): implement spawn_pause_menu function

--- src/state/pause.rs
+++ src/state/pause.rs
@@ -2,3 +2,29 @@ use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct PauseMenu;
+
+pub fn spawn_pause_menu(mut commands: Commands) {
+    commands.spawn((
+        PauseMenu,
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        },
+        BackgroundColor(Color::srgba(0.0, 0.0, 0.0, 0.7)),
+    )).with_children(|parent| {
+        parent.spawn((
+            Text::new("PAUSED\n\nPress ESC to resume"),
+            TextFont {
+                font_size: 36.0,
+                ..default()
+            },
+            TextColor(Color::WHITE),
+            TextLayout::new_with_justify(Justify::Center),
+        ));
+    });
+    
+    info!("Pause menu spawned");
+}
