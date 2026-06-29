743bd1b22d6b210108a136f2c233dd4224fcec96
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-29 12:26:37 +0000

    feat(state): implement from_velocity method for Facing

--- src/characters/facing.rs
+++ src/characters/facing.rs
@@ -10,3 +10,13 @@ pub enum Facing {
     Down,
     Right,
 }
+
+impl Facing {
+    pub fn from_velocity(velocity: Vec2) -> Self {
+        if velocity.x.abs() > velocity.y.abs() {
+            if velocity.x > 0.0 { Facing::Right } else { Facing::Left }
+        } else {
+            if velocity.y > 0.0 { Facing::Up } else { Facing::Down }
+        }
+    }
+}
