45e93452d40ef13652bd1833c48699bc477b7eff
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-29 12:52:10 +0000

    refactor(characters): update CharactersPlugin

--- src/characters/mod.rs
+++ src/characters/mod.rs
@@ -1,9 +1,12 @@
 pub mod state;
+pub mod input;
 pub mod spawn;
 pub mod config;
 pub mod facing;
-pub mod movement;
+pub mod physics;
 pub mod animation;
+
+use crate::state::GameState;
 
 use bevy::prelude::*;
 use bevy_common_assets::ron::RonAssetPlugin;
@@ -14,14 +17,15 @@ pub struct CharactersPlugin;
 impl Plugin for CharactersPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RonAssetPlugin::<CharactersList>::new(&["characters.ron"]))
-            .init_resource::<spawn::CurrentCharacterIndex>()
-            .add_systems(Startup, spawn::spawn_player)
-            .add_systems(Update, (
-                spawn::switch_character,
-                movement::move_player,
-                movement::update_jump_state,
-                animation::animate_characters,
-                animation::update_animation_flags,
-            ));
+        .init_resource::<spawn::CurrentCharacterIndex>()
+        .add_systems(Startup, spawn::spawn_player)
+        .add_systems(Update, (
+            input::handle_player_input,
+            spawn::switch_character,
+            input::update_jump_state,
+            animation::on_state_change_update_animation,
+            physics::apply_velocity,
+            animation::animations_playback,
+        ).chain().run_if(in_state(GameState::Playing)));
     }
 }
