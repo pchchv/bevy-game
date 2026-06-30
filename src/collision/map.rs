40c92f4da2cccbb97bd55150bdd325c63b643f25
Evgeni Pochechuev <ipchchv@gmail.com>
2026-06-30 05:51:41 +0000

    feat(collision): implement width and height methods for CollisionMap

--- src/collision/map.rs
+++ src/collision/map.rs
@@ -31,6 +31,12 @@ impl CollisionMap {
         }
     }
 
+    #[cfg(debug_assertions)]
+    pub fn width(&self) -> i32 { self.width }
+    
+    #[cfg(debug_assertions)]
+    pub fn height(&self) -> i32 { self.height }
+    
     /// Convert 2D grid coordinates to 1D array index.
     #[inline]
     fn xy_to_idx(&self, x: i32, y: i32) -> usize {
