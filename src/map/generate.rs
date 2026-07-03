5a27372a14765c34e66ad20048ddeaa90faf37c3
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-03 12:12:06 +0000

    refactor: delete local constants and add import

--- src/map/generate.rs
+++ src/map/generate.rs
@@ -1,16 +1,13 @@
 use bevy::prelude::*;
 use bevy_procedural_tilemaps::prelude::*;
+
+use crate::config::map::{GRID_X, GRID_Y, NODE_SIZE_Z, TILE_SIZE};
 use crate::map::{assets::{load_assets, prepare_tilemap_handles}, rules::build_world};
 
 // Configurable values
-/// Modify these values to control the map size.
-pub const GRID_X: u32 = 25;
-pub const GRID_Y: u32 = 18;
 
 const ASSETS_PATH: &str = "tile_layers";
 const TILEMAP_FILE: &str = "tilemap.png";
-/// Size of a block in world units (in Bevy 2d, 1 pixel is 1 world unit)
-pub const TILE_SIZE: f32 = 32.;
 /// Size of a grid node in world units
 const NODE_SIZE: Vec3 = Vec3::new(TILE_SIZE, TILE_SIZE, 1.);
 
