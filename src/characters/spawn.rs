4a071edf5d37a84fe326ac0d1d45cb3726127d37
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-09 11:56:21 +0000

    refactor(characters): remove spawn_player and initialize_player_character functions

--- src/characters/spawn.rs
+++ src/characters/spawn.rs
@@ -1,13 +1,13 @@
 use bevy::prelude::*;
-use crate::characters::animation::*;
-use crate::characters::config::{CharacterEntry, CharactersList};
-use crate::characters::input::Player;
-use crate::characters::state::CharacterState;
-use crate::characters::physics::Velocity;
-use crate::characters::facing::Facing;
-use crate::characters::collider::Collider;
-use crate::config::player::{PLAYER_SCALE, PLAYER_Z_POSITION};
 use crate::combat::PlayerCombat;
+use crate::characters::animation::*;
+use crate::characters::input::Player;
+use crate::characters::facing::Facing;
+use crate::characters::physics::Velocity;
+use crate::characters::collider::Collider;
+use crate::characters::state::CharacterState;
+use crate::config::player::{PLAYER_SCALE, PLAYER_Z_POSITION};
+use crate::characters::config::{CharacterEntry, CharactersList};
 
 #[derive(Resource, Default)]
 pub struct CurrentCharacterIndex {
@@ -31,70 +31,6 @@ fn create_character_atlas_layout(atlas_layouts: &mut ResMut<Assets<TextureAtlasL
     ))
 }
 
-pub fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>, mut character_index: ResMut<CurrentCharacterIndex>) {
-    // Load the characters list
-    let characters_list_handle: Handle<CharactersList> = asset_server.load("characters/characters.ron");
-    // Store the handle in a resource
-    commands.insert_resource(CharactersListResource {
-        handle: characters_list_handle,
-    });
-    
-    // Initialize with first character
-    character_index.index = 0;
-    // Spawn player entity (will be initialized once asset loads)
-    commands.spawn((
-        Player,
-        Transform::from_translation(Vec3::new(0.0, 0.0, PLAYER_Z_POSITION))
-            .with_scale(Vec3::splat(PLAYER_SCALE)),
-        Sprite::default(),
-    ));
-}
-
-pub fn initialize_player_character(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-    characters_lists: Res<Assets<CharactersList>>,
-    character_index: Res<CurrentCharacterIndex>,
-    characters_list_res: Option<Res<CharactersListResource>>,
-    mut query: Query<Entity, (With<Player>, Without<AnimationController>)>,
-) {
-    let Some(characters_list_res) = characters_list_res else {
-        return;
-    };
-    for entity in query.iter_mut() {
-        let Some(characters_list) = characters_lists.get(&characters_list_res.handle) else {
-            continue;
-        };
-        
-        if character_index.index >= characters_list.characters.len() {
-            continue;
-        };
-        
-        let character_entry = &characters_list.characters[character_index.index];
-        let texture = asset_server.load(&character_entry.texture_path);
-        let layout = create_character_atlas_layout(&mut atlas_layouts, character_entry);
-        let sprite = Sprite::from_atlas_image(
-            texture,
-            TextureAtlas {
-                layout,
-                index: 0,
-            },
-        );
-        
-        commands.entity(entity).insert((
-            AnimationController::default(),
-            CharacterState::default(),
-            Velocity::default(),
-            Facing::default(),
-            Collider::default(),
-            PlayerCombat::default(),
-            AnimationTimer(Timer::from_seconds(DEFAULT_ANIMATION_FRAME_TIME, TimerMode::Repeating)),
-            character_entry.clone(),
-            sprite,
-        ));
-    }
-}
 
 pub fn switch_character(
     input: Res<ButtonInput<KeyCode>>,
