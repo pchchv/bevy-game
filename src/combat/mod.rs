a81f973d552ab8bb643e2175aa9421732b02c452
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-14 15:29:11 +0000

    feat: integrate the healthbar module

--- src/combat/mod.rs
+++ src/combat/mod.rs
@@ -1,5 +1,6 @@
-mod player_combat;
 mod power_type;
+mod player_combat;
+pub mod healthbar;
 pub mod systems;
 pub mod health;
 
@@ -10,11 +11,23 @@ pub use power_type::{PowerType, PowerVisuals};
 pub use systems::{debug_switch_power, handle_power_input, spawn_projectile};
 
 use bevy::prelude::*;
+use crate::state::GameState;
 
 pub struct CombatPlugin;
 
 impl Plugin for CombatPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (handle_power_input, debug_switch_power));
+        app
+            .add_systems(
+                Update,
+                (
+                    handle_power_input,
+                    debug_switch_power,
+                    healthbar::spawn_healthbars,
+                    healthbar::update_healthbars,
+                )
+                    .chain()
+                    .run_if(in_state(GameState::Playing)),
+            );
     }
 }
