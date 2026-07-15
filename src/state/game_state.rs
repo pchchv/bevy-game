efbaf99551247a04c9145a557e4bfb0d719e2d9b
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-15 14:59:16 +0000

    feat(state): add GameOver to GameState

--- src/state/game_state.rs
+++ src/state/game_state.rs
@@ -3,7 +3,8 @@ use bevy::prelude::*;
 #[derive(States, Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum GameState {
     #[default]
+    Paused,
     Loading,
     Playing,
-    Paused,
+    GameOver,
 }
