00c05aebb14bc3e24e3021fa0504b720c9b43f34
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-20 05:31:12 +0000

    feat(save): add PlayerSave

--- src/save/data.rs
+++ src/save/data.rs
@@ -1,7 +1,21 @@
 use serde::{Serialize, Deserialize};
 
+use crate::combat::PowerType;
+use crate::characters::facing::Facing;
+
 #[derive(Serialize, Deserialize)]
 pub struct SaveFile {
     pub checksum: u64,
     pub data: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PlayerSave {
+    pub position: [f32; 3],
+    pub health_current: f32,
+    pub health_max: f32,
+    pub power_type: PowerType,
+    pub character_name: String,
+    pub character_index: usize,
+    pub facing: Facing,
 }
