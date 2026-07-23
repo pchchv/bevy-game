c8a6d61da593a64c303c02625e3a3d8ec88c8ec8
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-23 06:37:09 +0000

    feat(audio): implement AudioManagerPlugin

--- src/audio/mod.rs
+++ src/audio/mod.rs
@@ -1,2 +1,23 @@
 mod music;
 mod assets;
+
+use crate::state::GameState;
+use bevy::prelude::*;
+
+pub struct AudioManagerPlugin;
+
+impl Plugin for AudioManagerPlugin {
+    fn build(&self, app: &mut App) {
+        app
+            // Start loading assets and music on app startup
+            .add_systems(
+                Startup,
+                (assets::load_audio_assets, music::start_menu_music).chain(),
+            )
+            
+            // Music follows game state.
+            .add_systems(OnEnter(GameState::MainMenu), music::start_menu_music)
+            .add_systems(OnEnter(GameState::Playing), music::start_battle_music)
+            .add_systems(OnEnter(GameState::GameOver), music::stop_all_music);
+    }
+}
