12f9836e1f1ce6c78f5b6dec693f411fbe699017
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-23 17:19:03 +0000

    feat(audio): implement on_sfx function

--- src/audio/sfx.rs
+++ src/audio/sfx.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use crate::combat::PowerType;
+use super::assets::AudioAssets;
 
 #[derive(Event, Clone, Copy)]
 pub enum SfxKind {
@@ -11,4 +12,22 @@ pub enum SfxKind {
     Pickup,
     ButtonClick,
     Jump,
+}
+
+pub fn on_sfx(trigger: On<SfxKind>, audio_assets: Option<Res<AudioAssets>>, mut commands: Commands) {
+    let Some(audio) = audio_assets else {
+        return
+    };
+    let handle = match *trigger.event() {
+        SfxKind::PlayerShoot(PowerType::Fire) => audio.spell_fire.clone(),
+        SfxKind::PlayerShoot(_) => audio.spell_generic.clone(),
+        SfxKind::EnemyShoot => audio.enemy_shoot.clone(),
+        SfxKind::Hit => audio.hit.clone(),
+        SfxKind::PlayerDeath => audio.player_death.clone(),
+        SfxKind::EnemyDeath => audio.enemy_death.clone(),
+        SfxKind::Pickup => audio.pickup.clone(),
+        SfxKind::ButtonClick => audio.button_click.clone(),
+        SfxKind::Jump => audio.jump.clone(),
+    };
+    commands.spawn((AudioPlayer::new(handle), PlaybackSettings::DESPAWN));
 }
