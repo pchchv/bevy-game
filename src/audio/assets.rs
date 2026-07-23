8c2549e03b1502d6b71d4564021168b7c4b0b08b
Evgeni Pochechuev <ipchchv@gmail.com>
2026-07-23 06:21:12 +0000

    feat(audio): implement load_audio_assets function

--- src/audio/assets.rs
+++ src/audio/assets.rs
@@ -14,3 +14,19 @@ pub struct AudioAssets {
     pub button_click: Handle<AudioSource>,
     pub jump: Handle<AudioSource>,
 }
+
+pub fn load_audio_assets(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(AudioAssets {
+        menu_music: asset_server.load("audio/music/menu_ambient.mp3"),
+        battle_music: asset_server.load("audio/music/battle_theme.ogg"),
+        spell_generic: asset_server.load("audio/sfx/spell_01.ogg"),
+        spell_fire: asset_server.load("audio/sfx/spell_fire.wav"),
+        enemy_shoot: asset_server.load("audio/sfx/spell_enemy.ogg"),
+        hit: asset_server.load("audio/sfx/hit.wav"),
+        player_death: asset_server.load("audio/sfx/death_player.mp3"),
+        enemy_death: asset_server.load("audio/sfx/death_enemy.mp3"),
+        pickup: asset_server.load("audio/sfx/pickup.wav"),
+        button_click: asset_server.load("audio/sfx/button_click.wav"),
+        jump: asset_server.load("audio/sfx/jump.wav"),
+    });
+}
